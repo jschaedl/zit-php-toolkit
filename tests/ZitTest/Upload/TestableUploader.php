cce2482d3c38d269df6df5f0a1b71e7c582e12a4
Jan Schaedlich <schaedlich.jan@gmail.com>
2013-09-20 12:20:01 +0200

    Fixed formatting

--- tests/ZitTest/Upload/TestableUploader.php
+++ tests/ZitTest/Upload/TestableUploader.php
@@ -1,21 +1,21 @@
 <?php
 
 namespace ZitTest\Upload;
+
 use Zit\Upload\Uploader;
 
 /**
- * This class is used to override some methods which 
+ * This class is used to override some methods which
  * are not functional in cli environments.
  */
 class TestableUploader extends Uploader
-{   
-    protected function is_uploaded_file($filename)
-    {
+{
+
+    protected function is_uploaded_file($filename) {
         return file_exists($filename);
     }
 
-    protected function move_uploaded_file($filename, $destination)
-    {
+    protected function move_uploaded_file($filename, $destination) {
         return copy($filename, $destination);
     }
 }
